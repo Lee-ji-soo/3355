20777e368c8fd6a3a68835fa92316a076f1b7a9f
Lee-ji-soo <nbsp3@naver.com>
2023-11-23 00:47:43 +0900

    add Kakao Login

---
+++ app/auth/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+function Auth() {
+  return <h1>{'LOGIN SUCCESS'}</h1>;
+}
+
+export default Auth;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
+'use client';
+import KakaoLoginProvider from '../src/auth/kakao/KakaoLogin.provider';
+import { KakaoLoginComponents } from '../src/auth/kakao/KakaoLogin.components';
+
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
+
 function Home() {
-  return <h1>{'삼삼오오'}</h1>;
+  return (
+    <KakaoLoginProvider>
+      <KakaoLoginComponents.SigninButton />
+      <h1>{'삼삼오오'}</h1>;
+    </KakaoLoginProvider>
+  );
 }
 
 export default Home;
