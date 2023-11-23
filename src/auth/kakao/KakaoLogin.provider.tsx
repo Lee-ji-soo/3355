b96ff276ebc4d6398c79257d16d8fe6639d5f75b
Lee-ji-soo <nbsp3@naver.com>
2023-11-23 09:00:56 +0900

    script onload

--- src/auth/kakao/KakaoLogin.provider.tsx
+++ src/auth/kakao/KakaoLogin.provider.tsx
@@ -1,5 +1,5 @@
-'use client';
-import { ReactNode, useEffect } from 'react';
+"use client";
+import { ReactNode, useEffect } from "react";
 
 type KakaoLoginProviderProps = {
   children: ReactNode;
@@ -7,28 +7,27 @@ type KakaoLoginProviderProps = {
 
 const KakaoLoginProvider = (props: KakaoLoginProviderProps) => {
   useEffect(() => {
-    const head = document.getElementsByTagName('head').item(0);
-    const kakaoScript = document.createElement('script');
-    kakaoScript.setAttribute('type', 'text/javascript');
+    if (window.Kakao) return;
+    const head = document.getElementsByTagName("head").item(0);
+    const kakaoScript = document.createElement("script");
+    kakaoScript.setAttribute("type", "text/javascript");
     kakaoScript.setAttribute(
-      'src',
-      `https://t1.kakaocdn.net/kakao_js_sdk/${process.env.NEXT_PUBLIC_KAKAO_JDK_VERSION}/kakao.min.js`,
+      "src",
+      `https://t1.kakaocdn.net/kakao_js_sdk/2.5.0/kakao.min.js`
     );
     kakaoScript.setAttribute(
-      'integrity',
-      `${process.env.NEXT_PUBLIC_KAKAO_JDK_INTEGRITY_VALUE}`,
+      "integrity",
+      `${process.env.NEXT_PUBLIC_KAKAO_JDK_INTEGRITY_VALUE}`
     );
-    kakaoScript.setAttribute('crossorigin', 'anonymous');
+    kakaoScript.setAttribute("crossorigin", "anonymous");
     head?.appendChild(kakaoScript);
-  }, []);
-
-  useEffect(() => {
-    if (window.Kakao) {
+    kakaoScript.onload = () => {
       console.log(`kakao::`);
       window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY);
       console.log(`kakao::${window.Kakao.isInitialized()}`);
-    }
-  }, [window.Kakao]);
+    };
+  }, []);
+
   return <>{props.children}</>;
 };
 
